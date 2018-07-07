049c9ab641496eae71d3064b72c3149fd627f0d3
Jordan Day <jordan.day@tuta.io>
2018-07-07 15:23:25 +1000

    working on workshop 1 TODO: Task 5 	- Issues with importing TODO: Task 6: 	- code academy Functions
    Changes to be committed:
    	modified:   week01/code/index.js
    	modified:   week01/code/package.json

--- week01/code/index.js
+++ week01/code/index.js
@@ -1,12 +1,21 @@
+// Task 5 
+// TODO: NOT WORKING
+import { helloWorld } from 'hello-world-npm';
+
+console.log("This is task 5:");
+console.log(helloWorld());
+
 
 // Task 2
-console.log("Hello World");
+console.log("\nThis is task 2:");
+console.log("\tHello World");
 
 var myString = "Hello@World, Node.js Stringsss";
 var badWord = "bad word";
 var mistype = "Spwlling";
 
 // Task 4
+console.log("\nThis is task 4:");
 if (badWord == "bad word") {
 	badWord = "****";
 }
@@ -14,9 +23,16 @@ if (badWord == "bad word") {
 console.log(myString + " " + badWord + " " + mistype);
 
 // Task 3
+console.log("\nThis is task 3:");
 myString = myString.replace("@", " "); // replace @ with <space>
 myString = myString.slice(0,-2); // remove 2 s on the end
 
 console.log(myString); // Output
 
-// TODO: Task 5 & 6
+// TODO: Task 6
+/* Progress:
+	[x] Introduction to JavaScript
+	[x] Variables
+	[x] Control Flow
+	[ ] Functions
+ */
